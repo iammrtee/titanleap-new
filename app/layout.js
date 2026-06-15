e2ebbd6a78fad62fd5c423ff13fcef0036c8b544
Admin <tazrt37@gmail.com>
2026-06-15 18:12:21 +0100

    Update site

--- app/layout.js
+++ app/layout.js
@@ -16,6 +16,8 @@ export default function RootLayout({ children }) {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Archivo:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400&family=JetBrains+Mono:wght@400;500&display=swap" rel="stylesheet" />
         <meta name="theme-color" content="#080314" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/favicon.svg" />
       </head>
       <body>
         <ThemeProvider>
